49339bd7abdc561c85016e8fae3d5b0ccf261e72
Wang Zhengdong <hxroyal@gmail.com>
2022-03-21 15:27:10 +0800

    add info package.

---
+++ gateway/src/main/java/io/pivotal/spring/cloud/gateway/info/ApiKeyInfoContributor.java
@@ -0,0 +1,30 @@
+package io.pivotal.spring.cloud.gateway.info;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Component
+public class ApiKeyInfoContributor implements InfoContributor {
+    @Value("${apiKey.enabled:false}")
+    boolean apiKeyEnabled;
+    @Value("${apiKey.keysFilePath:}")
+    String keysFilePath;
+
+    public ApiKeyInfoContributor() {
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("apikey", Map.of(
+                "enabled", this.apiKeyEnabled,
+                "loaded", Files.exists(Paths.get(this.keysFilePath), new LinkOption[0])
+                ));
+    }
+}

d3c1bf830fd47f8e349d369835553c8226d4622d
Wang Zhengdong <hxroyal@gmail.com>
2022-03-21 16:41:05 +0800

    add parameter package.

---
+++ gateway/src/main/java/io/pivotal/spring/cloud/gateway/parameter/AllowedRequestQueryParamsCountGatewayFilterFactory.java
@@ -0,0 +1,46 @@
+package io.pivotal.spring.cloud.gateway.parameter;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AllowedRequestQueryParamsCountGatewayFilterFactory extends AbstractGatewayFilterFactory<AllowedRequestQueryParamsCountGatewayFilterFactory.Config> {
+    public AllowedRequestQueryParamsCountGatewayFilterFactory() {
+        super(AllowedRequestQueryParamsCountGatewayFilterFactory.Config.class);
+    }
+
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("paramsCount");
+    }
+
+    public GatewayFilter apply(AllowedRequestQueryParamsCountGatewayFilterFactory.Config config) {
+        return (exchange, chain) -> {
+            if (exchange.getRequest().getQueryParams().size() > config.getParamsCount()) {
+                exchange.getResponse().setStatusCode(HttpStatus.URI_TOO_LONG);
+                exchange.getResponse().getHeaders().set("errorMessage", "Request exceeded the maximum number of allowed query parameters");
+                return exchange.getResponse().setComplete();
+            } else {
+                return chain.filter(exchange);
+            }
+        };
+    }
+
+    static class Config {
+        private int paramsCount;
+
+        Config() {
+        }
+
+        public int getParamsCount() {
+            return this.paramsCount;
+        }
+
+        public void setParamsCount(int paramsCount) {
+            this.paramsCount = paramsCount;
+        }
+    }
+}

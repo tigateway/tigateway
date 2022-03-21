e26c9687fced695adc045b82cf7ac0d2542a12cc
Wang Zhengdong <hxroyal@gmail.com>
2022-03-21 15:29:53 +0800

    add ip package.

---
+++ gateway/src/main/java/io/pivotal/spring/cloud/gateway/ip/StoreIpAddressGatewayFilterFactory.java
@@ -0,0 +1,51 @@
+package io.pivotal.spring.cloud.gateway.ip;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.support.ipresolver.RemoteAddressResolver;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class StoreIpAddressGatewayFilterFactory extends AbstractGatewayFilterFactory<StoreIpAddressGatewayFilterFactory.Config> {
+    public StoreIpAddressGatewayFilterFactory() {
+        super(StoreIpAddressGatewayFilterFactory.Config.class);
+    }
+
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("parameterName");
+    }
+
+    public GatewayFilter apply(Config config) {
+        RemoteAddressResolver remoteAddressResolver = new RemoteAddressResolver() {
+        };
+        return (exchange, chain) -> {
+            InetSocketAddress remoteAddress = remoteAddressResolver.resolve(exchange);
+            if (remoteAddress != null) {
+                exchange.getAttributes().put(config.getParameterName(), remoteAddress.getAddress().getHostAddress());
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+
+    static class Config {
+        private String parameterName;
+
+        Config() {
+        }
+
+        public String getParameterName() {
+            return this.parameterName;
+        }
+
+        public void setParameterName(String parameterName) {
+            this.parameterName = parameterName;
+        }
+    }
+
+}

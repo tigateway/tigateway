1c93c6dd93f294192f60d38119b52323a3b38ba6
Wang Zhengdong <hxroyal@gmail.com>
2022-08-01 13:44:39 +0800

    move to ti-gateway component.

---
+++ ti-gateway/ti-gateway-storage/ti-gateway-storage-db/src/main/java/ti/gateway/storage/db/DbAppServerStorage.java
@@ -0,0 +1,47 @@
+package ti.gateway.storage.db;
+
+import org.springframework.util.CollectionUtils;
+import ti.gateway.core.cache.AppServer;
+import ti.gateway.core.cache.AppServerStorage;
+import ti.gateway.storage.db.mapper.GwAppInfoMapper;
+import ti.gateway.storage.db.mapper.GwAppServerMapper;
+import ti.gateway.storage.db.model.GwAppInfo;
+import ti.gateway.storage.db.model.GwAppServer;
+import ti.gateway.util.BeanCopierUtils;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class DbAppServerStorage implements AppServerStorage {
+
+    private GwAppInfoMapper gwAppInfoMapper;
+
+    private GwAppServerMapper gwAppServerMapper;
+
+    public DbAppServerStorage(GwAppInfoMapper gwAppInfoMapper, GwAppServerMapper gwAppServerMapper) {
+        this.gwAppInfoMapper = gwAppInfoMapper;
+        this.gwAppServerMapper = gwAppServerMapper;
+    }
+
+    /**
+     * 生成AppServer
+     *
+     * @param appId
+     * @return
+     */
+    @Override
+    public AppServer generateAppServer(String appId) {
+        GwAppInfo gwAppInfo = gwAppInfoMapper.selectByAppkey(appId);
+        if (Objects.nonNull(gwAppInfo)) {
+            AppServer appServer = BeanCopierUtils.copierTargetBean(gwAppInfo, GwAppInfo.class, AppServer.class);
+            Set<GwAppServer> gwAppServers = gwAppServerMapper.selectListByAppkey(appId);
+            if (!CollectionUtils.isEmpty(gwAppServers)) {
+                Set<AppServer.Server> servers = BeanCopierUtils.copierTargetBeanSet(gwAppServers, GwAppServer.class, AppServer.Server.class);
+                appServer.setServers(servers);
+            }
+            return appServer;
+        }
+        return null;
+    }
+
+}

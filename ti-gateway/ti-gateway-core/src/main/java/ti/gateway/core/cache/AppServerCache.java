8ade8293de29d80ed55f1b89f36e5426c0a2d1a7
Wang Zhengdong <hxroyal@gmail.com>
2022-08-01 13:44:09 +0800

    move to ti-gateway component.

---
+++ ti-gateway/ti-gateway-core/src/main/java/ti/gateway/core/cache/AppServerCache.java
@@ -0,0 +1,13 @@
+package ti.gateway.core.cache;
+
+public interface AppServerCache {
+
+    /**
+     * 获取应用服务信息
+     *
+     * @param appId
+     * @return
+     */
+    AppServer get(String appId);
+
+}

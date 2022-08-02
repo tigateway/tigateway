3210a1b76c664967586b4b460d89c0cd1e4bddd8
Wang Zhengdong <hxroyal@gmail.com>
2022-08-02 11:14:09 +0800

    修改包名

--- scg-operator/src/main/java/com/vmware/tanzu/springcloudgateway/route/UriBuildingException.java
+++
@@ -1,9 +0,0 @@
-package com.vmware.tanzu.springcloudgateway.route;
-
-import io.kubernetes.client.openapi.ApiException;
-
-public class UriBuildingException extends Exception {
-    public UriBuildingException(String message, ApiException e) {
-        super(message, e);
-    }
-}

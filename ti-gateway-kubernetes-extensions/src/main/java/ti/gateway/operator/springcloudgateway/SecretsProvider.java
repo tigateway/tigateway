3c0aea979aeb7c4bca8c6152811e3ce765b30244
Wang Zhengdong <hxroyal@gmail.com>
2022-08-02 11:16:51 +0800

    修改包名

---
+++ ti-gateway-kubernetes-extensions/src/main/java/ti/gateway/operator/springcloudgateway/SecretsProvider.java
@@ -0,0 +1,25 @@
+package ti.gateway.operator.springcloudgateway;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SecretsProvider {
+    private final LocalVaultFileReader reader;
+
+    public SecretsProvider(LocalVaultFileReader reader) {
+        this.reader = reader;
+    }
+
+    public Map<String, Map<String, Object>> getSecrets() {
+        return this.reader.getIds().stream()
+                .collect(Collectors.toMap((id) -> {
+                    return id;
+                }, (id) -> {
+                    return this.reader.getSecret(id);
+                }));
+    }
+
+    public Map<String, Object> getSecret(String secretId) {
+        return this.reader.getSecret(secretId);
+    }
+}
---
+++ ti-gateway-kubernetes-extensions/src/main/java/ti/gateway/operator/springcloudgateway/SecretsProvidersLocator.java
@@ -0,0 +1,43 @@
+package ti.gateway.operator.springcloudgateway;
+
+import java.nio.file.Path;
+import java.time.Clock;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SecretsProvidersLocator {
+    private final Logger log = LoggerFactory.getLogger(SecretsProvidersLocator.class);
+    private final Clock clock;
+    private final Map<String, SecretsProvider> providers = new HashMap<>();
+    private static final List<String> RESERVED_PATHS = List.of("api-portal-for-vmware-tanzu", "jwt-keys-for-vmware-tanzu");
+
+    public SecretsProvidersLocator(List<Path> secretsPaths, Clock clock) {
+        this.clock = clock;
+        List<Path> paths = (List<Path>)secretsPaths.stream().filter((path) -> {
+            return !this.containsAny(path, RESERVED_PATHS);
+        }).collect(Collectors.toList());
+        paths.forEach((path) -> {
+            String providerKey = path.getFileName().toString();
+            if (!this.providers.containsKey(providerKey)) {
+                this.providers.put(providerKey, new SecretsProvider(new LocalVaultFileReader(path, clock)));
+            } else {
+                this.log.info("Ignoring duplicated Secret Provider name: " + providerKey);
+            }
+
+        });
+    }
+
+    private boolean containsAny(Path path, List<String> reservedPaths) {
+        return reservedPaths.stream().anyMatch((reservedPath) -> {
+            return path.toAbsolutePath().toString().contains(reservedPath);
+        });
+    }
+
+    public SecretsProvider getSecretProvider(String name) {
+        return (SecretsProvider)this.providers.get(name);
+    }
+}

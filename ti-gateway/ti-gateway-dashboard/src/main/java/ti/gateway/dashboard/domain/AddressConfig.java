6e8f879b31ef9d7ca9184e49cde14620814014f3
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:13:04 +0800

    创建AddressConfig实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/AddressConfig.java
@@ -0,0 +1,18 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:09
+ */
+public class AddressConfig {
+    private String address;
+    private ForwardedHeaders forwardedHeaders;
+    private Http http;
+    private String name;
+    private ProxyProtocol proxyProtocol;
+    private Transport transport;
+
+    // Getters and setters
+}
+

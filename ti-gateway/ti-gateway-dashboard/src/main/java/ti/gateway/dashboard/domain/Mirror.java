6e119231bf22c890ace69e1453450461aa24d4b4
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:37:04 +0800

    create Mirror object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Mirror.java
@@ -0,0 +1,14 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:34
+ */
+public class Mirror {
+    private String name;
+    private int percent;
+
+    // Getters and setters
+}
+

ee203d86c7f0da9cf925b997ac7a3cdcc7d4987f
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:13:58 +0800

    创建ProxyProtocol实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/ProxyProtocol.java
@@ -0,0 +1,15 @@
+package ti.gateway.dashboard.domain;
+
+import java.util.List;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:10
+ */
+public class ProxyProtocol {
+    private boolean insecure;
+    private List<String> trustedIPs;
+
+    // Getters and setters
+}

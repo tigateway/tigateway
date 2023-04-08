4edd88c063afd7a2069bd494b0647f2f7b795d62
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:20:28 +0800

    创建LoadBalancer实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/LoadBalancer.java
@@ -0,0 +1,14 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:16
+ */
+public class LoadBalancer {
+    private List<Server> servers;
+    private boolean passHostHeader;
+
+    // Getters and setters
+}
+

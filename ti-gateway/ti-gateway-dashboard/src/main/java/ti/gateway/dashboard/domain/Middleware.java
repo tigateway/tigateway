de2a4bd8ef9950ebdf95e387d8646f629bf1d680
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:20:41 +0800

    创建Middleware实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Middleware.java
@@ -0,0 +1,16 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:15
+ */
+public class Middleware {
+    private AddPrefix addPrefix;
+    private BasicAuth basicAuth;
+    private String status;
+    private List<String> usedBy;
+
+    // Getters and setters
+}
+

06ffd4f9357e3e7e245f6481d55f5ee6ddbb6a88
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:13:18 +0800

    创建ForwardedHeaders实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/ForwardedHeaders.java
@@ -0,0 +1,15 @@
+package ti.gateway.dashboard.domain;
+
+import java.util.List;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:09
+ */
+public class ForwardedHeaders {
+    private boolean insecure;
+    private List<String> trustedIPs;
+
+    // Getters and setters
+}

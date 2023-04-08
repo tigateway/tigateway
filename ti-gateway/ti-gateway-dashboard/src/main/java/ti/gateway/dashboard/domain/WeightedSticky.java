3757eaba4051c4a09b2933a1cef79a297e1af261
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:38:15 +0800

    create WeightedSticky object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/WeightedSticky.java
@@ -0,0 +1,13 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:35
+ */
+public class WeightedSticky {
+    private StickyCookie cookie;
+
+    // Getters and setters
+}
+

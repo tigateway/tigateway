f5c8ee97e16c3ea12a873eb4d5b9f81f08203194
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:21:17 +0800

    创建Service实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Service.java
@@ -0,0 +1,15 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:16
+ */
+public class Service {
+    private LoadBalancer loadBalancer;
+    private String status;
+    private List<String> usedBy;
+
+    // Getters and setters
+}
+

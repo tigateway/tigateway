a8a968457a7ed0d57a3667b13eb8e4bcd0c13145
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:37:16 +0800

    create Mirroring object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Mirroring.java
@@ -0,0 +1,16 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:35
+ */
+import java.util.List;
+
+public class Mirroring {
+    private List<Mirror> mirrors;
+    private String service;
+
+    // Getters and setters
+}
+

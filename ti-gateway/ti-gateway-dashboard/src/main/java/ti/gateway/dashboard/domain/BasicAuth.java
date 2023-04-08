72e4e2b5a8ceadf5bf3247d307a37b8fdf9a88a3
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:19:39 +0800

    创建BasicAuth实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/BasicAuth.java
@@ -0,0 +1,13 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:16
+ */
+public class BasicAuth {
+    private List<String> users;
+
+    // Getters and setters
+}
+

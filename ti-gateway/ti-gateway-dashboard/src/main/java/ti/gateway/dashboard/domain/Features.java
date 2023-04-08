139ab1455b7cb68f518867d4cde3dce47b9287d7
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:27:37 +0800

    create Features object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Features.java
@@ -0,0 +1,16 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:25
+ */
+public class Features {
+    private boolean accessLog;
+    private String metrics;
+    private String tracing;
+    private boolean hub;
+
+    // Getters and setters
+}
+

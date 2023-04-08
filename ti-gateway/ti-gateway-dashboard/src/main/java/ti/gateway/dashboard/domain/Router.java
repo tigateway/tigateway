228387653079a9e105cc6fd621b47737aca85909
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:20:54 +0800

    创建Router实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Router.java
@@ -0,0 +1,19 @@
+package ti.gateway.dashboard.domain;
+
+import java.util.List;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:15
+ */
+public class Router {
+    private List<String> entryPoints;
+    private List<String> middlewares;
+    private String service;
+    private String rule;
+    private String status;
+
+    // Getters and setters
+}
+

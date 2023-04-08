573c3268b5183372364a67288b1ea9c0c4318208
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:28:21 +0800

    create StatusInfo object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/StatusInfo.java
@@ -0,0 +1,16 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:25
+ */
+public class StatusInfo {
+    private Features features;
+    private Http http;
+    private Tcp tcp;
+    private Udp udp;
+
+    // Getters and setters
+}
+

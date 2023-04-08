f044f173924daf3f5aa6184c4d389eb095adeb3c
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:19:27 +0800

    创建AddPrefix实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/AddPrefix.java
@@ -0,0 +1,13 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:15
+ */
+public class AddPrefix {
+    private String prefix;
+
+    // Getters and setters
+}
+

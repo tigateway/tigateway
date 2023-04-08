187c84f3adced18e0a354b9fb6369594c49af9f7
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:13:44 +0800

    创建LifeCycle实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/LifeCycle.java
@@ -0,0 +1,14 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:10
+ */
+public class LifeCycle {
+    private String graceTimeOut;
+    private String requestAcceptGraceTimeout;
+
+    // Getters and setters
+}
+

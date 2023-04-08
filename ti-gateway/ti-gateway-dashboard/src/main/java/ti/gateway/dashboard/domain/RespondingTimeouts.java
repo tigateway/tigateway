2c93ba1334fde1b8dff5f880a7141c7f304ff368
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:14:13 +0800

    创建RespondingTimeouts实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/RespondingTimeouts.java
@@ -0,0 +1,15 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:10
+ */
+public class RespondingTimeouts {
+    private String idleTimeout;
+    private String readTimeout;
+    private String writeTimeout;
+
+    // Getters and setters
+}
+

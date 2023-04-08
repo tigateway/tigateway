7689af12763d6fb9a23d1d3d4350a765dcd2252f
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:44:32 +0800

    create LoadBalancerInfo object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/LoadBalancerInfo.java
@@ -0,0 +1,20 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:44
+ */
+import java.util.List;
+
+public class LoadBalancerInfo {
+    private LoadBalancer loadBalancer;
+    private String name;
+    private String provider;
+    private String status;
+    private String type;
+    private List<String> usedBy;
+
+    // Getters and setters
+}
+

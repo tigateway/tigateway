87b56f6f7f60099d3635d8666456fdfac4fcc265
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:28:30 +0800

    create Tcp object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Tcp.java
@@ -0,0 +1,15 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:26
+ */
+public class Tcp {
+    private ItemStatus middlewares;
+    private ItemStatus routers;
+    private ItemStatus services;
+
+    // Getters and setters
+}
+

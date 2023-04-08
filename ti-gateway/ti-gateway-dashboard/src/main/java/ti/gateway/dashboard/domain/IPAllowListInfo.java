b4c8b2eeae612580082684142938dde8b78b4fbd
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:40:15 +0800

    create IPAllowListInfo object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/IPAllowListInfo.java
@@ -0,0 +1,20 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:39
+ */
+import java.util.List;
+
+public class IPAllowListInfo {
+    private IPAllowList ipAllowList;
+    private String name;
+    private String provider;
+    private String status;
+    private String type;
+    private List<String> usedBy;
+
+    // Getters and setters
+}
+

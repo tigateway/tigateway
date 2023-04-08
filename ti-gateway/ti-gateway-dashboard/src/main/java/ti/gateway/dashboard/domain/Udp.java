818a5c5dee573a85bb2aa338d7b338470e71012d
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:28:41 +0800

    create Udp object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Udp.java
@@ -0,0 +1,14 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:26
+ */
+public class Udp {
+    private ItemStatus routers;
+    private ItemStatus services;
+
+    // Getters and setters
+}
+

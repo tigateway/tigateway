b2af57c9649179e3ba2608618db3b02266d03a8b
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:14:25 +0800

    创建Transport实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Transport.java
@@ -0,0 +1,14 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:11
+ */
+public class Transport {
+    private LifeCycle lifeCycle;
+    private RespondingTimeouts respondingTimeouts;
+
+    // Getters and setters
+}
+

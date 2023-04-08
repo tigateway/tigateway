cc1f8d3350dd0bf5bc4aab0f7bd8f9a93486465d
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:32:47 +0800

    create RouterInfo object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/RouterInfo.java
@@ -0,0 +1,22 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:32
+ */
+import java.util.List;
+
+public class RouterInfo {
+    private List<String> entryPoints;
+    private List<String> middlewares;
+    private String name;
+    private String provider;
+    private String rule;
+    private String service;
+    private String status;
+    private List<String> using;
+
+    // Getters and setters
+}
+

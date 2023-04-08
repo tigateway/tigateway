1b16e20c8d3c84f5b2af6af029be33aa0ad15e0c
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:37:27 +0800

    create ServerStatus object.

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/ServerStatus.java
@@ -0,0 +1,15 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:36
+ */
+import java.util.Map;
+
+public class ServerStatus {
+    private Map<String, String> serverStatus;
+
+    // Getters and setters
+}
+

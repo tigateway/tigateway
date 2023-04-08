7043287a94bc0687887402d49965b080a9e82dba
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:21:29 +0800

    创建TcpRouter实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/TcpRouter.java
@@ -0,0 +1,16 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:17
+ */
+public class TcpRouter {
+    private List<String> entryPoints;
+    private String service;
+    private String rule;
+    private String status;
+
+    // Getters and setters
+}
+

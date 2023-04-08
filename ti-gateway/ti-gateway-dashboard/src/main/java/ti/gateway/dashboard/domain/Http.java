3bbc821de205c7e4133c17744fb95e319b9ff462
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:27:48 +0800

    update Http object.

--- ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Http.java
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Http.java
@@ -6,4 +6,10 @@ package ti.gateway.dashboard.domain;
  * @date 2023/4/9 01:10
  */
 public class Http {
+    private ItemStatus middlewares;
+    private ItemStatus routers;
+    private ItemStatus services;
+
+    // Getters and setters
 }
+

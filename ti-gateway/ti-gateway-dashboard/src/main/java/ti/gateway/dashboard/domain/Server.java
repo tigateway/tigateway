b8998cc691f60ac09e837adc64beeb7d97095e1f
Wang Zhengdong <hxroyal@gmail.com>
2023-04-09 01:21:05 +0800

    创建Server实体

---
+++ ti-gateway/ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/domain/Server.java
@@ -0,0 +1,14 @@
+package ti.gateway.dashboard.domain;
+
+/**
+ * @author wangzhengdong
+ * @version 1.0
+ * @date 2023/4/9 01:17
+ */
+public class Server {
+    private String url;
+    private String address;
+
+    // Getters and setters
+}
+

100c969e7e8afb9697222dc2cfce065c6fa65f08
Wang Zhengdong <hxroyal@gmail.com>
2024-04-15 01:11:17 +0800

    增加新的admin模块

---
+++ ti-gateway/ti-gateway-admin/src/main/java/ti/gateway/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package ti.gateway.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"ti.gateway"})
+@MapperScan(value = "ti.gateway.storage.db.mapper")
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
---
+++ ti-gateway/ti-gateway-admin/src/main/java/ti/gateway/admin/storage/db/mapper/Mybatis.java
@@ -0,0 +1,15 @@
+package ti.gateway.storage.db.mapper;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * Mybatis Component
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Mybatis {
+}

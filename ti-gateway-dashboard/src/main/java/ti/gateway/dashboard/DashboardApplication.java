0b56dea567f3c629c14b9962173a3918a03130eb
Wang Zhengdong <hxroyal@gmail.com>
2024-04-15 01:49:58 +0800

    update import class path.

--- ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/DashboardApplication.java
+++ ti-gateway-dashboard/src/main/java/ti/gateway/dashboard/DashboardApplication.java
@@ -1,11 +1,9 @@
 package ti.gateway.dashboard;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"ti.gateway"})
-@MapperScan(value = "ti.gateway.admin.storage.db.mapper")
 public class DashboardApplication {
 
     public static void main(String[] args) {
